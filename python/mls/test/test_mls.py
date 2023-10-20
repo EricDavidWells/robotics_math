ab2abdcca523a6e6bdf786ba85da16107bcf44ea
eric <eric@sanctuary.ai>
2023-10-19 22:30:04 -0700

    simpler test

--- python/mls/test/test_mls.py
+++ python/mls/test/test_mls.py
@@ -20,7 +20,6 @@ def test_skew_symmetric_matrix():
     with pytest.raises(ValueError):
         skew_symmetric_matrix(invalid_input_vector)
 
-
 def test_create_homogeneous_xform_with_valid_input():
     # Test with valid input
     rotation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
@@ -34,15 +33,13 @@ def test_create_homogeneous_xform_with_valid_input():
 
     assert np.array_equal(result, expected_result)
 
-def test_create_homogeneous_xform_with_invalid_rotation():
-    # Test with invalid input (rotation matrix not 3x3)
-    rotation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]])
-    translation = np.array([1, 2, 3])
+    # Test with invalid input (neither rotation nor translation is a NumPy array)
+    rotation = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    translation = [1, 2, 3]
 
     with pytest.raises(ValueError):
         create_homogeneous_xform(rotation, translation)
 
-def test_create_homogeneous_xform_with_invalid_translation():
     # Test with invalid input (translation vector not 3x1)
     rotation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     translation = np.array([1, 2])
@@ -50,10 +47,9 @@ def test_create_homogeneous_xform_with_invalid_translation():
     with pytest.raises(ValueError):
         create_homogeneous_xform(rotation, translation)
 
-def test_create_homogeneous_xform_with_invalid_input_type():
-    # Test with invalid input (neither rotation nor translation is a NumPy array)
-    rotation = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    translation = [1, 2, 3]
+    # Test with invalid input (rotation matrix not 3x3)
+    rotation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]])
+    translation = np.array([1, 2, 3])
 
     with pytest.raises(ValueError):
         create_homogeneous_xform(rotation, translation)
